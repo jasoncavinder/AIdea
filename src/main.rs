22891e57228d391e0f9ec31d1f07dc45d6f8d314
Jason Cavinder <jason.cavinder@gmail.com>
2024-02-11 14:29:11 -1000

    Changed test deployment message

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use axum::{routing::get, Router};
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, World! Welcome to AIdea!" }));
 
     // run our app with hyper, listening globally on port 8080
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
